953637b8b7ee8d068024e4b21b84636b58c95fe2
Girard-Arnold <8736074+Girard-Arnold@users.noreply.github.com>
2020-01-30 12:45:48 +0100

    Update extract upn.ps1

--- extract upn.ps1
+++ extract upn.ps1
@@ -1,6 +1,7 @@
 ####Extraction des UPN dans une OU specifique et export vers un CSV
 $filepath = "$home\desktop"
 $filename = "resultupn.csv"
+# Le but notamment est de comparer le mail et l'UPN notamment dans le cadre d'une migration vers O365.
 get-aduser -filter * -searchbase "OU=users,OU=societe,DC=societe,DC=int" -properties * | select-object name, userprincipalname | export-csv -delimiter ";" -path "$path\$filename"
 ECHO "Resultat disponible dans "$filepath\$filename""
 ECHO "Fin de script"
