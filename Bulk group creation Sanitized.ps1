0d9ee8c0d0132b410d0a3d28ab99ea96eeb98d43
Girard-Arnold <8736074+Girard-Arnold@users.noreply.github.com>
2020-01-30 13:43:07 +0100

    Update Bulk group creation Sanitized.ps1

--- Bulk group creation Sanitized.ps1
+++ Bulk group creation Sanitized.ps1
@@ -1,6 +1,9 @@
 $groupname = Import-Csv -Path "$home\desktop\ftp.csv"
+$GroupOuPath = OU=Societe,OU=Groupes techniques,DC=domain,DC=TLD
 foreach($group in $groupname){
 
-    new-adgroup -name $group.name -groupscope global -groupcategory security -path "GroupOUPath" -description "Donne acces au dossier $group sur le FTP"
+    new-adgroup -name $group.name -groupscope global -groupcategory security -path "$GroupOuPath" -description "Donne acces au dossier $group sur le FTP"
 
 }
+
+ECHO "Fin de script"
