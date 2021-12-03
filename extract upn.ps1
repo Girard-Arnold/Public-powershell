ed70d89ae498410ca869c2d9832a48bf1947d0a8
Girard-Arnold <8736074+Girard-Arnold@users.noreply.github.com>
2021-12-03 10:26:08 +0100

    Ajout -notypeinformation
    Parce que flemme de l'enlever à chaque fois.

--- extract upn.ps1
+++ extract upn.ps1
@@ -2,7 +2,7 @@
 $filepath = "$home\desktop"
 $filename = "resultupn.csv"
 # Le but notamment est de comparer le mail et l'UPN notamment dans le cadre d'une migration vers O365.
-get-aduser -filter * -searchbase "OU=users,OU=societe,DC=societe,DC=int" -properties * | select-object name, userprincipalname | export-csv -delimiter ";" -path "$path\$filename"
+get-aduser -filter * -searchbase "OU=users,OU=societe,DC=societe,DC=int" -properties * | select-object name, userprincipalname | export-csv -delimiter ";" -path "$path\$filename" -notypeinformation
 ECHO "Resultat disponible dans "$filepath\$filename""
 ECHO "Fin de script"
 END
