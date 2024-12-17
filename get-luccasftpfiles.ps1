14b82c6b34adf737938485a8d9a73d6b3df93446
Girard-Arnold <8736074+Girard-Arnold@users.noreply.github.com>
2024-12-17 18:22:58 +0100

    Update get-luccasftpfiles.ps1

--- get-luccasftpfiles.ps1
+++ get-luccasftpfiles.ps1
@@ -1,23 +1,23 @@
 ###
-#Declaring used variables for easy editing
-#Assemblypath points to the correct dll for the .net framework.
-#xmlpath is for the configuration file containing the credentials.
-#targetfolder is the destination folder for the Download
+# Declaring used variables for easy editing
+# Assemblypath points to the correct dll for the .net framework.
+# xmlpath is for the configuration file containing the credentials.
+# targetfolder is the destination folder for the Download
 ###
 
-$assemblyPath = "E:\Sources Appli\Script\WinSCP\WinSCPnet.dll"
-$xmlpath = "E:\Sources Appli\Script\WinSCP\config.xml"
-$targetfolder = "E:\Ftp LUCCA\"
+$assemblyPath = "path\to\WinSCPnet.dll"
+$xmlpath = "path\to\config.xml"
+$targetfolder = "targetfolderpath"
 # Load the correct WinSCP .NET assembly for PowerShell
 # This can be downloaded from WinSCP's website. Very important for a powershell script as per the documentation.
-$assemblyPath = "E:\Sources Appli\Script\WinSCP\WinSCPnet.dll"
+
 
 [Reflection.Assembly]::LoadFrom($assemblyPath)
 
 # Load XML file with credentials
 $config = [xml](Get-Content "$xmlpath")
 
-# Extract the username and password. The password is in the path above, is encrypted and can only be decrypted from the account reportone.
+# Extract the username and password. The password is in the path above, is encrypted and can only be decrypted from the account used to encrypt.
 $username = $config.Configuration.UserName
 $encryptedPassword = $config.Configuration.Password
 
@@ -29,10 +29,10 @@ $plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto(
 # Set up WinSCP session options for SFTP.
 $sessionOptions = New-Object WinSCP.SessionOptions
 $sessionOptions.Protocol = [WinSCP.Protocol]::Sftp
-$sessionOptions.HostName = "ftp.eu1.luccasoftware.com"
+$sessionOptions.HostName = "ftpserver" # Don't need to put sftp: at the start of the url
 $sessionOptions.UserName = $username
 $sessionOptions.Password = $plainPassword
-$sessionOptions.SshHostKeyFingerprint = "ssh-ed25519 255 Ddex0NLLm4ifi7pZK8h3gkN1EVcEq3LuYZTwVottyno" # Replace 'sshkey' with the actual SSH key fingerprint
+$sessionOptions.SshHostKeyFingerprint = "sshkey" # Replace 'sshkey' with the actual SSH key fingerprint
 
 # Initialize the WinSCP session
 $session = New-Object WinSCP.Session
